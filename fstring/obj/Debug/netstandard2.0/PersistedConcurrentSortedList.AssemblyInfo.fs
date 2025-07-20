bc953e80fd3d5fdd3bb09f5ea1fc25d736a1cffc
anibal <anibal.yeh@gmail.com>
2025-07-20 14:04:51 +0800

    net9_300

--- fstring/obj/Debug/netstandard2.0/PersistedConcurrentSortedList.AssemblyInfo.fs
+++ fstring/obj/Debug/netstandard2.0/PersistedConcurrentSortedList.AssemblyInfo.fs
@@ -10,9 +10,9 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("PersistedConcurrentSortedList")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyDescriptionAttribute("SortedList with persistency and threadsafty.")>]
-[<assembly: System.Reflection.AssemblyFileVersionAttribute("9.0.27.16")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("9.0.27.16+afb75367c539dbd980cfbc189839ea81a1284721")>]
+[<assembly: System.Reflection.AssemblyFileVersionAttribute("9.0.27.316")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("9.0.27.316+0453a513e8b0e29ab31f1932e8f439422965c74c")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("PersistedConcurrentSortedList")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("PersistedConcurrentSortedList")>]
-[<assembly: System.Reflection.AssemblyVersionAttribute("9.0.27.16")>]
+[<assembly: System.Reflection.AssemblyVersionAttribute("9.0.27.316")>]
 do()
